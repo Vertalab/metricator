92195ebeb35ad3b138a396da67ebcec34ce0293d
kiosan <sasha@vertalab.com>
2017-09-30 00:02:41 +0300

    deleted

--- app/controllers/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-class SessionsController < Devise::SessionsController
-  after_action :prepare_intercom_shutdown, only: [:destroy]
-
-  protected
-
-  def prepare_intercom_shutdown
-    IntercomRails::ShutdownHelper.prepare_intercom_shutdown(session)
-  end
-end
--- app/controllers/visitors_controller.rb
+++
@@ -1,9 +0,0 @@
-class VisitorsController < ApplicationController
-  after_action :intercom_shutdown, only: [:index]
-
-  protected
-
-  def intercom_shutdown
-    IntercomRails::ShutdownHelper.intercom_shutdown(session, cookies)
-  end
-end
