acaa640964ed9cd75d0b5ac27cfec892e46a1f99
kiosan <sasha@vertalab.com>
2017-10-26 23:25:35 +0300

    work with 0

--- app/helpers/metrics_helper.rb
+++ app/helpers/metrics_helper.rb
@@ -8,6 +8,7 @@ module MetricsHelper
   end
 
   def show_delta_percents(m)
-    content_tag(:span, "#{m.recent_value.delta > 0 ? '+' : m.recent_value.delta == 0 ? '' : '-'} #{m.recent_value.delta_percent.to_i}%", class: "label #{m.up? && m.recent_value.delta > 0 || m.down? && m.recent_value.delta < 0 ? 'label-success' : m.recent_value.delta == 0 ? 'label-default' : 'label-danger'}")
+    percent = m.recent_value.delta_percent * 100 / 100.0
+    content_tag(:span, "#{m.recent_value.delta > 0 ? '+' : m.recent_value.delta == 0 ? '' : '-'}#{m.recent_value.delta != 0 ? m.recent_value.delta : ''} (#{percent}%)", class: "label #{m.up? && m.recent_value.delta > 0 || m.down? && m.recent_value.delta < 0 ? 'label-success' : m.recent_value.delta == 0 ? 'label-default' : 'label-danger'}")
   end
 end
