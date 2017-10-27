e8489e6d1082e8457bc18210766f006d5ca7b9e0
kiosan <sasha@vertalab.com>
2017-10-27 12:13:56 +0300

    ceil percents

--- app/helpers/metrics_helper.rb
+++ app/helpers/metrics_helper.rb
@@ -8,7 +8,7 @@ module MetricsHelper
   end
 
   def show_delta_percents(m)
-    percent = m.recent_value.delta_percent * 100 / 100.0
+    percent = (m.recent_value.delta_percent * 100).to_i / 100.0
     content_tag(:span, "#{m.recent_value.delta > 0 ? '+' : m.recent_value.delta == 0 ? '' : ''}#{m.recent_value.delta != 0 ? m.recent_value.delta : ''} #{m.recent_value.delta != 0 ? '/' : ''} #{percent}%", class: "label #{m.up? && m.recent_value.delta > 0 || m.down? && m.recent_value.delta < 0 ? 'label-success' : m.recent_value.delta == 0 ? 'label-default' : 'label-danger'}")
   end
 end
